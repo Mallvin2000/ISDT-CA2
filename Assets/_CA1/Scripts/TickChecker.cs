91f29ba6b6b01f6bc31522f615ff97c81bb837ef
Mallvin2000 <64111704+Mallvin2000@users.noreply.github.com>
2021-01-11 11:38:24 +0800

    importing CA1 folder structure

---
+++ Assets/_CA1/Scripts/TickChecker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//-----------------------------------------------------------------------------------------------------
+//Author: Arrashi
+//Description of code: When a object enters a snapzone, this script is called to enable the tick on the clipboard
+//-----------------------------------------------------------------------------------------------------
+
+public class TickChecker : MonoBehaviour
+{
+
+    public Material CompleteMaterial;
+    public GameObject Tick1;
+    public GameObject Tick2;
+    public GameObject Tick3;
+
+    public bool Check1 = false;
+    public bool Check2 = false;
+    public bool Check3 = false;
+
+
+    public void enableTick(string zoneName)// enable tick when object enters snapzone
+    {
+        if (zoneName == "wetFloorSign")
+        {
+            Debug.Log(zoneName);
+            Tick1.GetComponent<MeshRenderer>().material = CompleteMaterial;
+            Check1 = true;
+        }
+    }
+
+    public void checkIfComplete()//when submit button is pressed, check to see if all tasks are completed
+    {
+        if (Check1 == true)
+        {
+            Debug.Log("win");
+            //Application.LoadLevel("Win");
+        }
+    }
+
+
+    private void Start()
+    {
+        Debug.Log("start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // checkIfComplete();
+    }
+}
---
+++ Assets/_CA1/Scripts/checkWinCondition.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//-----------------------------------------------------------------------------------------------------
+//Author: Mallvin Rajamohan
+//Description of code: To check if all taks are completed and then changes scene to the Win scene
+//-----------------------------------------------------------------------------------------------------
+
+public class checkWinCondition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void checkIfAllCompleted()
+    {
+        if (GameObject.Find("taskList").GetComponent<TickChecker>().Check1 == true)//if task completed
+        {
+            //Debug.Log(GameObject.Find("taskList").GetComponent<TickChecker>().Check1);
+            Application.LoadLevel("Win");//change to win scene
+        } else
+        {
+            Debug.Log("not completed yet");
+        }
+        
+    }
+}
