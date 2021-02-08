5c2d92d5a738f1e584d39ef55795117478d57eda
Mallvin2000 <64111704+Mallvin2000@users.noreply.github.com>
2021-02-08 11:11:55 +0800

    loading game works

--- Assets/_CA1/Scripts/DataManager.cs
+++ Assets/_CA1/Scripts/DataManager.cs
@@ -9,15 +9,23 @@ public class DataManager : MonoBehaviour
 {
     //public ActData ActData;
     //saving game mechanic completion data
-    public bool wetFloorSignCompleted = GameObject.Find("clipboard").GetComponent<TickChecker>().Check1;
-    public bool safetyHarnessCompleted = GameObject.Find("clipboard").GetComponent<TickChecker>().Check2;
-    public bool wiresCompleted = GameObject.Find("clipboard").GetComponent<TickChecker>().Check3;
+    public bool wetFloorSignCompleted;
+    public bool safetyHarnessCompleted;
+    public bool wiresCompleted;
     // Start is called before the first frame update
     void Start()
     {
-       // Debug.Log(ActData.wetFloorSignCompleted);
+        
+        // Debug.Log(ActData.wetFloorSignCompleted);
         //Save();
-        Read();
+        //Read();
+    }
+
+    public void getDataToSave()
+    {
+        wetFloorSignCompleted = GameObject.Find("Clipboard").GetComponent<TickChecker>().Check1;
+        safetyHarnessCompleted = GameObject.Find("Clipboard").GetComponent<TickChecker>().Check2;
+        wiresCompleted = GameObject.Find("Clipboard").GetComponent<TickChecker>().Check3;
     }
 
     // Update is called once per frame
@@ -28,11 +36,13 @@ public class DataManager : MonoBehaviour
 
     public void Save()
     {
+        getDataToSave();
         string path = "Assets/test.txt";
         string json = "{\"wetFloorSignCompleted\":\""+wetFloorSignCompleted+"\",\"safetyHarnessCompleted\":\""+safetyHarnessCompleted+"\",\"wiresCompleted\":\""+wiresCompleted+"\"}";
 
         //Write some text to the test.txt file
         StreamWriter writer = new StreamWriter(path, true);
+        writer.WriteLine();
         writer.WriteLine(JsonUtility.ToJson(this));
         writer.Close();
     }
@@ -40,13 +50,25 @@ public class DataManager : MonoBehaviour
     public void Read()
     {
           string path = "Assets/test.txt";
+           string line;
+           List<string> Lines = new List<string>();
 
         //Read the text from directly from the test.txt file
-        StreamReader reader = new StreamReader(path); 
-        //Debug.Log(reader.ReadToEnd());
-        string json = reader.ReadToEnd();
-        //string[] words = json.Split(',');
-        //string[] boolInfo;
+        StreamReader reader = new StreamReader(path);
+        //string json = reader.ReadToEnd();
+
+        //store all lines in txt file in list
+        while ((line = reader.ReadLine()) != null)
+        {
+            //Debug.Log(line);
+            Lines.Add(line);
+        }
+        //get last line in list for the latest save
+        int i = Lines.Count - 1;
+        string json = Lines[i];
+        //Debug.Log(i);
+        //Debug.Log(Lines[i]);
+
         string[] toPush;
         List<string> boolInfo = new List<string>();
         boolInfo = json.Split(',').ToList();
@@ -59,20 +81,30 @@ public class DataManager : MonoBehaviour
         //converting to boolean
         string stringName = WetFloorRead[1];
         bool wetFloorBool = Convert.ToBoolean(stringName);
+        //Debug.Log(wetFloorBool);
 
         SafetyHarnessRead = boolInfo[1].Split(':').ToList();
         //converting to boolean
         string stringName2 = SafetyHarnessRead[1];
         bool safteyHarnessBool = Convert.ToBoolean(stringName2);
+        //Debug.Log(safteyHarnessBool);
 
         WiresRead = boolInfo[2].Split(':').ToList();
+        List<string> WiresReadFinal = new List<string>();
+        WiresReadFinal = WiresRead[1].Split('}').ToList();
         //converting to boolean
-        string stringName3 = WiresRead[1];
+        string stringName3 = WiresReadFinal[0];
+        //Debug.Log(stringName3);
         bool wiresBool = Convert.ToBoolean(stringName3);
+        //Debug.Log(wiresBool);
 
         reader.Close();
 
+        loadSavedData(wetFloorBool, safteyHarnessBool, wiresBool);
+
     }
+
+   
 
     public void  loadSavedData(bool wetFloorBool, bool safteyHarnessBool, bool wiresBool)
     {
